24bdb602f7a21ac2c43915ada078526c9b7dbd76
Jair T <157268413+aviangenomics@users.noreply.github.com>
2024-04-11 10:50:27 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -155,7 +155,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'pastel',
+    defaultTheme: 'coffee',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
